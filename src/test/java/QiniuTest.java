8609a3ed486af0e7aa3cb5c5db74a5e99cd4220e
yzjla123 <yzjla@qq.com>
2017-04-22 16:03:08 +0800

    test

--- src/test/java/QiniuTest.java
+++ src/test/java/QiniuTest.java
@@ -7,7 +7,7 @@ public class QiniuTest {
 		String secretKey = "DCM_Lo2U9Oyact4BE262YVYzu8-ior9tQ-lYb4CR";
 		Auth auth = Auth.create(accessKey, secretKey);
 		String uploadToken = auth.uploadToken("happycar-img");
-		System.out.println(uploadToken);
+		System.out.println(uploadToken+"");
 	}
 
 }
